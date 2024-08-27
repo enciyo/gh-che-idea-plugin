18d4f83c5f44d6fc406d82af9718b34ef1fbcacb
GitHub Action <action@github.com>
2024-08-27 04:05:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/enciyo/ghcheideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/enciyo/ghcheideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.enciyo.ghcheideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/enciyo/ghcheideaplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/enciyo/ghcheideaplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.enciyo.ghcheideaplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.enciyo.ghcheideaplugin.MyBundle
+import com.github.enciyo.ghcheideaplugin.services.MyProjectService
 import javax.swing.JButton
 
 
